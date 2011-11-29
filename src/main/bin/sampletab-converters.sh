a3a88ffcb786678596a11bd866e3d6596a2eb644
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2011-11-29 13:36:55 +0000

    Added debugging output for shell

--- src/main/bin/sampletab-converters.sh
+++ src/main/bin/sampletab-converters.sh
@@ -6,11 +6,17 @@ current=`pwd`;
 java=/ebi/research/software/Linux_x86_64/opt/java/jdk1.7.0/bin/java;#
 args="-Dhttp.proxyHost=wwwcache.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=*.ebi.ac.uk -DproxyHost=wwwcache.ebi.ac.uk -DproxyPort=3128 -DproxySet=true";
 
+
 for file in `ls $base/lib`
 do
   jars=$jars:$base/lib/$file;
 done
 
 classpath="$jars:$base/config";
+
+
+echo "base is: $base"
+echo "currrent is: $current"
+echo "jars is $jars"
 
 $java $args -classpath $classpath $@
