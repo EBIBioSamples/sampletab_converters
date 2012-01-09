2e9827d7a4de06afd80ad4732f13e8ffa3393b9f
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-01-09 14:56:40 +0000

    Added shell scripts that are used by conan-biosd-processes.

---
+++ src/main/bin/PRIDEtrim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Do not modify this file directly
+#It is stored at svn://bar.ebi.ac.uk/trunk/fgpt//automation/sampletab-converters/src/main/bin
+#and installed by http://coconut.ebi.ac.uk:9081/browse/BSD-CONVS
+
+#Trim out the assay part of the PRIDE XML files
+#If this is not done, they are too large to be used effectively.
+
+gunzip -cd $1 | sed '/<GelFreeIdentification>/,/<\/GelFreeIdentification>/d' | sed '/<TwoDimensionalIdentification>/,/<\/TwoDimensionalIdentification>/d' > $2
