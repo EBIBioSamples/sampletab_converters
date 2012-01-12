9f01c400dae744dc114847b17b10a6afff837e2c
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-01-12 16:50:39 +0000

    Added class to be called from a cron script to manage PRIDE FTP downloading

---
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/PRIDEcron.java
@@ -0,0 +1,128 @@
+package uk.ac.ebi.fgpt.sampletab;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PRIDEcron {
+    private Logger log = LoggerFactory.getLogger(getClass());
+    // singlton instance
+    private static PRIDEcron instance = null;
+
+    private FTPClient ftp = null;
+
+    private PRIDEcron() {
+        // private constructor to prevent accidental multiple initialisations
+    }
+
+    public static PRIDEcron getInstance() {
+        if (instance == null) {
+            instance = new PRIDEcron();
+        }
+        return instance;
+    }
+
+    public FTPClient getFTPClient() throws IOException {
+        if (ftp == null) {
+            ftp = new FTPClient();
+            String server = "ftp.ebi.ac.uk";
+            int reply;
+            ftp.connect(server);
+            ftp.login("anonymous", "");
+
+            log.info("Connected to " + server + ".");
+            log.info(ftp.getReplyString());
+
+            // After connection attempt, check the reply code to verify success.
+            reply = ftp.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                throw new IOException("Unable to connect to ftp server " + server);
+            }
+            log.info("connected to FTP");
+        }
+        return ftp;
+    }
+
+    private void close() {
+        try {
+            ftp.logout();
+        } catch (IOException e) {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                    // do nothing
+                }
+            }
+        }
+
+    }
+
+    public void run(File outdir) {
+        FTPClient ftp;
+        FTPFile[] files;
+        try {
+            ftp = getFTPClient();
+            log.info("Getting file listing...");
+            files = ftp.listFiles("/pub/databases/pride/");
+            log.info("Got file listing");
+        } catch (IOException e) {
+            System.err.println("Unable to connect to FTP");
+            e.printStackTrace();
+            return;
+        }
+        Pattern regex = Pattern.compile("PRIDE_Exp_Complete_Ac_([0-9]+)\\.xml\\.gz");
+
+        Runtime runtime = Runtime.getRuntime();
+        //Executor pool = Executors.newFixedThreadPool(runtime.availableProcessors());
+        Executor pool = Executors.newFixedThreadPool(1);
+
+        for (FTPFile file : files) {
+            String filename = file.getName();
+            Matcher matcher = regex.matcher(filename);
+            if (matcher.matches()) {
+                String accession = matcher.group(1);
+                File outfile = new File(outdir, accession + ".xml");
+                pool.execute(new PRIDEFTPDownload(accession, outfile));
+            }
+        }
+
+        try {
+            synchronized (pool) {
+                pool.wait();
+            }
+        } catch (InterruptedException e) {
+            log.error("Interrupted waiting for FTP download pool to complete");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Must provide the following paramters:");
+            System.err.println("  PRIDE local directory");
+            return;
+        }
+        String path = args[0];
+        File outdir = new File(path);
+
+        if (outdir.exists() && !outdir.isDirectory()) {
+            System.err.println("Target is not a directory");
+            return;
+        }
+
+        if (!outdir.exists())
+            outdir.mkdirs();
+
+        getInstance().run(outdir);
+    }
+}
