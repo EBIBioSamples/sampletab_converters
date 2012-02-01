2ca3cdb16a98106af0872af1b61c2f9490c2c4e3
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-02-01 17:09:25 +0000

    Changed to manually constructing and destructing a BufferedFileReader

--- src/main/java/uk/ac/ebi/fgpt/sampletab/utils/XMLUtils.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/utils/XMLUtils.java
@@ -1,6 +1,10 @@
 package uk.ac.ebi.fgpt.sampletab.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -14,7 +18,7 @@ import org.dom4j.io.SAXReader;
 public class XMLUtils {
 
 	private static ConcurrentLinkedQueue<SAXReader> readerQueue = new ConcurrentLinkedQueue<SAXReader>();
-	
+
 	public static Document getDocument(File xmlFile) throws DocumentException{
 		SAXReader reader = readerQueue.poll();
 		if (reader == null){
@@ -23,40 +27,52 @@ public class XMLUtils {
 		
 		//now do actual parsing
 		Document xml = null;
+		Reader r = null;
 		try {
-			xml = reader.read(xmlFile);
+			r = new BufferedReader(new FileReader(xmlFile));
+			xml = reader.read(r);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
-		//return the reader back to the queue
+			if (r != null){
+				try {
+					r.close();
+				} catch (IOException e){
+					//do nothing
+				}
+			}
+			//return the reader back to the queue
 			reader.resetHandlers();
 			readerQueue.add(reader);
 		}
 		
 		return xml;
 	}
-	
-	public static Document getDocument(String xmlURL) throws DocumentException{
+
+	public static Document getDocument(String xmlURL) throws DocumentException {
 		SAXReader reader = readerQueue.poll();
-		if (reader == null){
+		if (reader == null) {
 			reader = new SAXReader();
 		}
-		
-		//now do actual parsing
+
+		// now do actual parsing
 		Document xml = null;
 		try {
 			xml = reader.read(xmlURL);
 		} finally {
-		//return the reader back to the queue
+			// return the reader back to the queue
 			reader.resetHandlers();
 			readerQueue.add(reader);
 		}
-		
+
 		return xml;
 	}
-	
+
 	public static Element getChildByName(Element parent, String name) {
 		if (parent == null)
 			return null;
-		
+
 		for (Iterator<Element> i = parent.elementIterator(); i.hasNext();) {
 			Element child = i.next();
 			if (child.getName().equals(name)) {
@@ -70,10 +86,10 @@ public class XMLUtils {
 	public static Collection<Element> getChildrenByName(Element parent,
 			String name) {
 		Collection<Element> children = new ArrayList<Element>();
-		
+
 		if (parent == null)
 			return children;
-		
+
 		for (Iterator<Element> i = parent.elementIterator(); i.hasNext();) {
 			Element child = i.next();
 			if (child.getName().equals(name)) {
