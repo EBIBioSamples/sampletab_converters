a5eadbb0b40f0c74d0f1ae373968990e5d2f5153
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-03-29 15:31:40 +0000

    Ensure files are group writable

--- src/main/bin/IMSRTabcron.sh
+++ src/main/bin/IMSRTabcron.sh
@@ -4,4 +4,7 @@
 #It is stored at svn://bar.ebi.ac.uk/trunk/fgpt//automation/sampletab-converters/src/main/bin
 #and installed by http://coconut.ebi.ac.uk:9081/browse/BSD-CONVS
 
+#ensure files are group writable
+umask 002
+
 ${0%/*}/sampletab-converters.sh uk.ac.ebi.fgpt.sampletab.IMSRTabcron "$@"
