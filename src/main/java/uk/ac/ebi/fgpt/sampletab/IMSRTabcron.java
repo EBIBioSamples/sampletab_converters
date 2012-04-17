bdac73b2cbff6be09399c4917ff4219cdeea3f5a
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-04-17 09:59:57 +0000

    Handle downloading only modifies IMSR files.

--- src/main/java/uk/ac/ebi/fgpt/sampletab/IMSRTabcron.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/IMSRTabcron.java
@@ -2,8 +2,10 @@ package uk.ac.ebi.fgpt.sampletab;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -65,28 +67,40 @@ public class IMSRTabcron {
 		if (!outdir.exists())
 			outdir.mkdirs();
 
+		IMSRTabWebSummary summary = IMSRTabWebSummary.getInstance();
+		try {
+            summary.get();
+        } catch (NumberFormatException e) {
+            System.err.println("Unable to download summary");
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        } catch (ParseException e) {
+            System.err.println("Unable to download summary");
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        } catch (IOException e) {
+            System.err.println("Unable to download summary");
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        }
+
         IMSRTabWebDownload downloader = new IMSRTabWebDownload();
-        downloader.download("GMS-JAX", new File(new File(outdir, "GMS-JAX"), "raw.tab.txt"));
-        downloader.download("GMS-HAR", new File(new File(outdir, "GMS-HAR"), "raw.tab.txt"));
-        downloader.download("GMS-MMRRC", new File(new File(outdir, "GMS-MMRRC"), "raw.tab.txt"));
-        downloader.download("GMS-ORNL", new File(new File(outdir, "GMS-ORNL"), "raw.tab.txt"));
-        downloader.download("GMS-CARD", new File(new File(outdir, "GMS-CARD"), "raw.tab.txt"));
-        downloader.download("GMS-EM", new File(new File(outdir, "GMS-EM"), "raw.tab.txt"));
-        downloader.download("GMS-NMICE", new File(new File(outdir, "GMS-NMICE"), "raw.tab.txt"));
-        downloader.download("GMS-RBRC", new File(new File(outdir, "GMS-RBRC"), "raw.tab.txt"));
-        downloader.download("GMS-NCIMR", new File(new File(outdir, "GMS-NCIMR"), "raw.tab.txt"));
-        downloader.download("GMS-CMMR", new File(new File(outdir, "GMS-CMMR"), "raw.tab.txt"));
-        downloader.download("GMS-APB", new File(new File(outdir, "GMS-APB"), "raw.tab.txt"));
-        downloader.download("GMS-EMS", new File(new File(outdir, "GMS-EMS"), "raw.tab.txt"));
-        downloader.download("GMS-HLB", new File(new File(outdir, "GMS-HLB"), "raw.tab.txt"));
-        downloader.download("GMS-NIG", new File(new File(outdir, "GMS-NIG"), "raw.tab.txt"));
-        downloader.download("GMS-TAC", new File(new File(outdir, "GMS-TAC"), "raw.tab.txt"));
-        downloader.download("GMS-MUGEN", new File(new File(outdir, "GMS-MUGEN"), "raw.tab.txt"));
-        downloader.download("GMS-TIGM", new File(new File(outdir, "GMS-TIGM"), "raw.tab.txt"));
-        downloader.download("GMS-KOMP", new File(new File(outdir, "GMS-KOMP"), "raw.tab.txt"));
-        downloader.download("GMS-RMRC-NLAC", new File(new File(outdir, "GMS-RMRC-NLAC"), "raw.tab.txt"));
-        downloader.download("GMS-OBS", new File(new File(outdir, "GMS-OBS"), "raw.tab.txt"));
-        downloader.download("GMS-WTSI", new File(new File(outdir, "GMS-WTSI"), "raw.tab.txt"));
+        for(int i = 0; i < summary.sites.size(); i++){
+            String site = summary.sites.get(i);
+            String subID = "GMS-"+site;
+            File raw = new File(new File(outdir, subID), "raw.tab.txt");
+            if (!raw.exists()){
+                downloader.download(subID, raw);
+            } else {
+                Date fileDate = new Date(raw.lastModified());
+                if (summary.updates.get(i).after(fileDate)){
+                    downloader.download(subID, raw);
+                }
+            }
+        }
         
 	}
 }
