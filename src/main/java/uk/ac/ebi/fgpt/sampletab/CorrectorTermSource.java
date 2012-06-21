78fa247d59e19c5e0b74371b73cdb68b34b8eb6b
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-06-21 16:03:33 +0000

    Improved missing / extra term soruce handling

--- src/main/java/uk/ac/ebi/fgpt/sampletab/CorrectorTermSource.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/CorrectorTermSource.java
@@ -33,6 +33,7 @@ public class CorrectorTermSource {
                 }
             }
         }
+        
         //now trim them from the msi
         //use a new list so original can be deleted from
         for (TermSource ts: new ArrayList<TermSource>(sampledata.msi.termSources)){
@@ -42,16 +43,131 @@ public class CorrectorTermSource {
             }
         }
         
+        //add to the msi any term source which samples use
+        for (String usedTsName : usedTsNames) {
+            boolean exists = false;
+            for (TermSource ts: new ArrayList<TermSource>(sampledata.msi.termSources)) {
+                if (usedTsName.equals(ts.getName())) {
+                    exists = true;
+                    break;
+                }
+            }
+            
+            if (!exists) {
+                TermSource ts = new TermSource(usedTsName, null, null);
+                sampledata.msi.termSources.add(ts);
+            }
+        }
+        
+        
         //in some cases, term sources have a null URL
         //for a sub-set of these we can guess them from being popular, e.g. EFO, NCBI Taxonomy, etc
+        //for others, delete them as they are not useful
         for (int i = 0; i < sampledata.msi.termSources.size(); i++){
             TermSource ts = sampledata.msi.termSources.get(i);
             if (ts.getURI() == null){
                 if (ts.getName().equals("EFO")){
                     ts = new TermSource(ts.getName(), "http://www.ebi.ac.uk/efo/", null);
                     sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("PAR")){
+                    ts = new TermSource(ts.getName(), "http://psidev.cvs.sourceforge.net/psidev/psi/mi/controlledVocab/proteomeBinder/psi-par.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("PSI")){
+                    ts = new TermSource(ts.getName(), "http://psidev.sourceforge.net/ms/xml/mzdata/psi-ms-cv-1.7.2.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("MeSH")){
+                    ts = new TermSource(ts.getName(), "http://www.nlm.nih.gov/mesh/", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("MP")){
+                    ts = new TermSource(ts.getName(), "ftp://ftp.informatics.jax.org/pub/reports/MPheno_OBO.ontology", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("PRIDE")){
+                    ts = new TermSource(ts.getName(), "http://pride-proteome.cvs.sourceforge.net/pride-proteome/PRIDE/schema/pride_cv.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("NEWT")){
+                    ts = new TermSource(ts.getName(), "http://www.ebi.ac.uk/newt/", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("GPM")){
+                    ts = new TermSource(ts.getName(), "http://www.thegpm.org/", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("GPMDB")){
+                    ts = new TermSource(ts.getName(), "http://gpmdb.thegpm.org/", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("MS")){
+                    ts = new TermSource(ts.getName(), "http://psidev.cvs.sourceforge.net/viewvc/*checkout*/psidev/psi/psi-ms/mzML/controlledVocabulary/psi-ms.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("CCO")){
+                    ts = new TermSource(ts.getName(), "http://cellcycleonto.cvs.sourceforge.net/cellcycleonto/ONTOLOGIES/OBO/cco.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("MOD")){
+                    ts = new TermSource(ts.getName(), "http://psidev.cvs.sourceforge.net/psidev/psi/mod/data/PSI-MOD.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("SEP")){
+                    ts = new TermSource(ts.getName(), "http://code.google.com/p/gelml/source/browse/trunk/CV/sep.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("DOID")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/phenotype/human_disease.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("CL")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/cell_type/cell.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("IDO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/phenotype/infectious_disease.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("EO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/phenotype/environment/environment_ontology.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("BTO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/BrendaTissue.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("GO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/genomic-proteomic/gene_ontology.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("PO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/developmental/plant_development/plant/po_temporal.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("TAIR")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/developmental/plant_development/Arabidopsis/arabidopsis_development.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("ZEA")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/gross_anatomy/plant_gross_anatomy/cereals/zea_mays_anatomy.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("CHEBI")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/chemical/chebi.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("UO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/phenotype/unit.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("ZFA")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/gross_anatomy/animal_gross_anatomy/fish/zebrafish_anatomy.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("TAO")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/gross_anatomy/animal_gross_anatomy/fish/teleost_anatomy.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else if (ts.getName().equals("DDANAT")){
+                    ts = new TermSource(ts.getName(), "http://obo.cvs.sourceforge.net/obo/obo/ontology/anatomy/gross_anatomy/microbial_gross_anatomy/dictyostelium/dictyostelium_anatomy.obo", null);
+                    sampledata.msi.termSources.set(i, ts);
+                } else {
+                    log.error("Removing null term source "+ts.getName());
+                    sampledata.msi.termSources.remove(i);
+                    //because the list is now shorter, adjust the counter too
+                    i--;
+                    //now remove any reference to this ontology on any node
+                    for (SCDNode scdnode : sampledata.scd.getAllNodes()){
+                        for (SCDNodeAttribute attr : scdnode.getAttributes()){
+                            if (AbstractNodeAttributeOntology.class.isInstance(attr)){
+                                AbstractNodeAttributeOntology attrOnt = (AbstractNodeAttributeOntology) attr;
+                                //if this attribute has this term source, remove it
+                                if (attrOnt.getTermSourceREF().equals(ts.getName())){
+                                    attrOnt.setTermSourceREF(null);
+                                    attrOnt.setTermSourceIDInteger(null);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+        //TODO add a date if no version present?
     }
 }
