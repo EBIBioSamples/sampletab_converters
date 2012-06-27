8d8b63c97b935475f5fd39cf16dc6dab2240c861
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-06-27 16:29:00 +0000

    Finished initial coding.

--- src/main/java/uk/ac/ebi/fgpt/sampletab/SampleTabStatus.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/SampleTabStatus.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -18,8 +18,10 @@ import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.Files;
+
 import uk.ac.ebi.fgpt.sampletab.utils.FileUtils;
-import uk.ac.ebi.fgpt.sampletab.utils.SampleTabStatusRunnable;
+import uk.ac.ebi.fgpt.sampletab.utils.ProcessUtils;
 import uk.ac.ebi.fgpt.sampletab.utils.SampleTabUtils;
 
 public class SampleTabStatus {
@@ -30,14 +32,165 @@ public class SampleTabStatus {
     @Argument(required=true, index=0, metaVar="FTP", usage = "ftp directory")
     private String ftpDirFilename;
 
-    @Argument(required=true, index=1, metaVar="INPUT", usage = "input filename or globs")
+    @Argument(required=true, index=1, metaVar="SCRIPT", usage = "script directory")
+    private String scriptDirFilename;
+
+    @Argument(required=true, index=2, metaVar="INPUT", usage = "input filename or globs")
     private List<String> inputFilenames;
     
     @Option(name = "--threaded", usage = "use multiple threads?")
     private boolean threaded = false;
 
+    @Option(name = "--agename", usage = "Age server hostname")
+    private String agename = null;
+
+    @Option(name = "--ageusername", usage = "Age server username")
+    private String ageusername = null;
+
+    @Option(name = "--agepassword", usage = "Age server password")
+    private String agepassword = null;
+
     private Logger log = LoggerFactory.getLogger(getClass());
 
+    public SampleTabStatus(){
+        Properties ageProperties = new Properties();
+        try {
+            ageProperties.load(SampleTabStatus.class.getResourceAsStream("/age.properties"));
+        } catch (IOException e) {
+            log.error("Unable to read resource age.properties");
+            e.printStackTrace();
+        }
+        this.agename = ageProperties.getProperty("hostname");
+        this.ageusername = ageProperties.getProperty("username");
+        this.agepassword = ageProperties.getProperty("password");
+    }
+    
+    private void toDatabase(List<File> inputFiles){
+        List<File> ageFiles = new ArrayList<File>();
+        for(File inputFile : inputFiles) {
+            log.info("Adding to database "+inputFile);
+            File ageDir = new File(inputFile, "age");
+            File ageFile = new File(ageDir, inputFile.getName()+".age.txt");
+            
+            File loadDirFile = new File(inputFile, "load");
+            File loadSuccessFile = new File(loadDirFile, inputFile.getName()+".SUCCESS");
+            
+            if (ageFile.exists()){
+                //if it has never been loaded, or it is loaded but out of date
+                if (!loadSuccessFile.exists() || ageFile.lastModified() > loadSuccessFile.lastModified()){
+                    ageFiles.add(ageDir);
+                }
+            }
+        }
+        File scriptDir = new File(scriptDirFilename);
+        File scriptFile = new File(scriptDir, "AgeTab-Loader.sh");
+        
+        File tempDir = Files.createTempDir();
+        
+        String mainCommand = scriptFile.getAbsolutePath() 
+            + " -s -m -i -e -mmode"
+            + " -o "+tempDir.getAbsolutePath()
+            + " -u "+ageusername
+            + " -p "+agepassword
+            + " -h \""+agename+"\""; 
+        
+        //because of shell maximum command length
+        //(which you can find by running 'getconf ARG_MAX')
+        //only process some files at a time
+        //approximate max length is at least 100,000 characters
+        
+        String commandRoot = mainCommand;
+        for(File ageDir : ageFiles){
+            if (mainCommand.length() + ageDir.getAbsolutePath().length()+1 < 100000){
+                mainCommand = mainCommand+" "+ageDir.getAbsolutePath();
+            } else {
+                //command is too long. run and reset.
+                ProcessUtils.doCommand(mainCommand, null);
+                mainCommand = commandRoot+" "+ageDir.getAbsolutePath();
+            }
+        }
+        
+        //now all of them should be loaded, but we have to put the load reports 
+        //back in the right directories.
+        for(File inputFile : inputFiles){
+            
+            File successFile = new File(tempDir, inputFile.getName()+".SUCCESS");
+            File errorFile = new File(tempDir, inputFile.getName()+".ERROR");
+            
+            File loadDirFile = new File(inputFile, "load");
+            File loadSuccessFile = new File(loadDirFile, inputFile.getName()+".SUCCESS");
+            File loadErrorFile = new File(loadDirFile, inputFile.getName()+".ERROR");
+            
+            if (successFile.exists()){
+                try {
+                    FileUtils.move(successFile, loadSuccessFile);
+                } catch (IOException e) {
+                    log.error("Unable to move "+successFile);
+                    e.printStackTrace();
+                }
+            }
+
+            if (errorFile.exists()){
+                try {
+                    FileUtils.move(errorFile, loadErrorFile);
+                } catch (IOException e) {
+                    log.error("Unable to move "+errorFile);
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        //and finally, clean up
+        //Note due to issues described in http://code.google.com/p/guava-libraries/issues/detail?id=365
+        //this is not totally secure against concurrent modification.
+        for (File tempFile : tempDir.listFiles()){
+            if (!tempFile.delete()){
+                log.error("Unable to delete "+tempFile);
+            }
+        }
+        if (!tempDir.delete()){
+            log.error("Unable to delete "+tempDir);
+        }
+        
+    }
+    
+    private void toFTP(List<File> inputFiles){
+        File ftpDir = new File(ftpDirFilename);
+        for(File inputFile : inputFiles){
+            log.info("Adding to ftp "+inputFile);
+            String accession = inputFile.getName();
+            File ftpSubDir = new File(ftpDir, SampleTabUtils.getPathPrefix(accession)); 
+            File ftpSubSubDir = new File(ftpSubDir, accession);
+            File ftpFile = new File(ftpSubSubDir, "sampletab.txt");
+            
+            File sampletabFile = new File(inputFile, "sampletab.txt");
+            
+            if (!ftpFile.exists() && sampletabFile.exists()){
+                try {
+                    FileUtils.copy(sampletabFile, ftpFile);
+                    ftpFile.setLastModified(sampletabFile.lastModified());
+                } catch (IOException e) {
+                    log.error("Unable to copy to FTP "+ftpFile);
+                    e.printStackTrace();
+                }
+            }
+            
+            //also need to try to remove private tag from database
+            //not sure what will happen if it doesn't have the tag
+            File scriptDir = new File(scriptDirFilename);
+            File scriptFile = new File(scriptDir, "TagControl.sh");
+
+            String command = scriptFile.getAbsolutePath() 
+                + " -u "+ageusername
+                + " -p "+agepassword
+                + " -h \""+agename+"\"" 
+                + " -r Security:Private"
+                + " -i "+inputFile.getName();
+
+            ProcessUtils.doCommand(command, null);
+        }
+    }
+    
     public static void main(String[] args) {
         new SampleTabStatus().doMain(args);
     }
@@ -106,6 +259,8 @@ public class SampleTabStatus {
         
         //remove from FTP
         for(File inputFile : SampleTabStatusRunnable.toRemoveFromFTP){
+            log.info("Removing from ftp "+inputFile);
+            
     		File ftpDir = new File(ftpDirFilename);
         	File ftpSubDir = new File(ftpDir, SampleTabUtils.getPathPrefix(inputFile.getName())); 
     		File ftpSubSubDir = new File(ftpSubDir, inputFile.getName());
@@ -119,30 +274,32 @@ public class SampleTabStatus {
         }
         
         //remove from database
-        //TODO finish
-        
-        
-        //copy to FTP
-        for(File inputFile : SampleTabStatusRunnable.toCopyToFTP){
-    		File ftpDir = new File(ftpDirFilename);
-        	File ftpSubDir = new File(ftpDir, SampleTabUtils.getPathPrefix(inputFile.getName())); 
-    		File ftpSubSubDir = new File(ftpSubDir, inputFile.getName());
-    		File ftpFile = new File(ftpSubSubDir, "sampletab.txt");
-    		
-    		File sampletabFile = new File(inputFile, "sampletab.txt");
-    		
-    		if (!ftpFile.exists() && sampletabFile.exists()){
-    			try {
-					FileUtils.copy(sampletabFile, ftpFile);
-				} catch (IOException e) {
-	    			log.error("Unable to copy to FTP "+ftpFile);
-					e.printStackTrace();
-				}
-    		}
+        //or rather "hide from the public"
+        for(File inputFile : SampleTabStatusRunnable.toRemoveFromDatabase){
+            log.info("Removing from database "+inputFile);
+
+            File scriptDir = new File(scriptDirFilename);
+            File scriptFile = new File(scriptDir, "TagControl.sh");
+
+            String command = scriptFile.getAbsolutePath() 
+                + " -u "+ageusername
+                + " -p "+agepassword
+                + " -h \""+agename+"\"" 
+                + " -a Security:Private"
+                + " -i "+inputFile.getName();
+
+            ProcessUtils.doCommand(command, null);
         }
         
         //add to database
-        //TODO finish
+        toDatabase(SampleTabStatusRunnable.toAddToDatabase);
+        
+        
+        //copy to FTP
+        toFTP(SampleTabStatusRunnable.toCopyToFTP);
+        //because tags may have changed, need to trigger re-indexing
+        //this could be done by going into and out of maintenence mode
+        //once that tool is finished
         
     }
 }
