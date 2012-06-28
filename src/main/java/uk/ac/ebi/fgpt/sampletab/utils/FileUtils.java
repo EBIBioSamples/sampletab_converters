c3ea2a3e24edeaeaa04098a7d0cdb802c5c64126
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2012-06-28 14:02:43 +0000

    Better checks on moving/copying files.

--- src/main/java/uk/ac/ebi/fgpt/sampletab/utils/FileUtils.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/utils/FileUtils.java
@@ -124,10 +124,20 @@ public class FileUtils {
     }
     
     public static void copy(File sourceFile, File destFile) throws IOException {
+        sourceFile = sourceFile.getAbsoluteFile();
+        destFile = destFile.getAbsoluteFile();
+        if (!destFile.getParentFile().mkdirs()){
+            throw new IOException("Unable to make directories for "+destFile);
+        }
         Files.copy(sourceFile, destFile);
     }
     
     public static void move(File sourceFile, File destFile) throws IOException {
+        sourceFile = sourceFile.getAbsoluteFile();
+        destFile = destFile.getAbsoluteFile();
+        if (!destFile.getParentFile().mkdirs()){
+            throw new IOException("Unable to make directories for "+destFile);
+        }
         Files.move(sourceFile, destFile);
     }
 }
