539db71f094ce6d8c4f3a4c74b289180cb0f6000
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-01-11 14:59:52 +0000

    First Euro PubMed Central API usage.

---
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/utils/europmc/ws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.cdb.ebi.ac.uk/")
+package uk.ac.ebi.fgpt.sampletab.utils.europmc.ws;
---
+++ src/test/java/uk/ac/ebi/fgpt/sampletab/TestEuroPMCUtils.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.fgpt.sampletab;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.ebi.fgpt.sampletab.utils.EuroPMCUtils;
+import uk.ac.ebi.fgpt.sampletab.utils.europmc.ws.QueryException_Exception;
+import junit.framework.TestCase;
+
+
+public class TestEuroPMCUtils extends TestCase {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    public void testBioSD() {
+        String title = null;
+        try {
+            title = EuroPMCUtils.getTitleByPUBMEDid(22096232);
+        } catch (QueryException_Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            fail();
+        }
+
+        assertEquals("The BioSample Database (BioSD) at the European Bioinformatics Institute.", title);
+    }
+
+}
