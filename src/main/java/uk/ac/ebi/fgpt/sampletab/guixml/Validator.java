8480b89006012c808505ccaf99ea6de09d5b8c43
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-03-15 16:35:58 +0000

    First pass at gui xml validator

---
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/guixml/Validator.java
@@ -0,0 +1,160 @@
+package uk.ac.ebi.fgpt.sampletab.guixml;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import uk.ac.ebi.fgpt.sampletab.AbstractInfileDriver;
+
+public class Validator extends AbstractInfileDriver {
+    
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    public class BiosamplesHandler extends DefaultHandler {
+        
+        private String groupID = null;
+        private final Collection<String> sampleattributesknown = new ArrayList<String>();
+        private boolean inSample = false;
+        private final Collection<String> sampleattributesseen = new HashSet<String>();
+        private final Collection<String> sampleattributesseensample = new ArrayList<String>();
+
+        public BiosamplesHandler() {
+            
+        }
+        
+        
+        @Override
+        public void startElement(String uri, String localName, String name, Attributes atts) throws SAXException {
+            if (name.equals("SampleGroup")) {
+                groupID = atts.getValue("id");
+                //check group ID
+                if (groupID == null) {
+                    throw new SAXException("missing group ID");
+                } else if (!groupID.matches("SAM[END][AG]?[1-9][0-9]{4,}")) {
+                    throw new SAXException(groupID+" is not a valid group ID");
+                }
+            } else if (name.equals("SampleAttributes")) {
+                if (sampleattributesknown.size() != 0) {
+                    throw new SAXException("Multiple SampleAttributes in "+groupID);
+                }
+            } else if (name.equals("SampleAttribute")) {
+                String className = atts.getValue("class");
+                if (className == null ) {
+                    throw new SAXException("Missing SampleAttribute class in "+groupID);
+                } else if (sampleattributesknown.contains(className)) {
+                    //check sampleAttributes contain no duplicates
+                    throw new SAXException("Duplicated SampleAttribute class "+className+" in "+groupID);
+                } else {
+                    sampleattributesknown.add(className);
+                }
+            } else if (name.equals("Sample")) {
+                inSample = true;
+                //check group id
+                String sampleGroupID = atts.getValue("groupId");
+                if (!groupID.equals(sampleGroupID)) {
+                    throw new SAXException("Mismatched groupId : "+groupID+" vs "+sampleGroupID);
+                }
+                //check sample id
+                String sampleID = atts.getValue("id");
+                if (!sampleID.matches("SAM[END][AG]?[1-9][0-9]{4,}")) {
+                    throw new SAXException(sampleID+" is not a valid sample ID");
+                }
+            } else if (name.equals("attribute") && inSample) {
+                String className = atts.getValue("class");
+                if (className == null ) {
+                    throw new SAXException("Missing SampleAttribute class in "+groupID);
+                } else if (className.equals("Term Source REF") 
+                        || className.equals("Term Source ID")
+                        || className.equals("Term Source Name")
+                        || className.equals("Term Source URI")) {
+                    //do nothing
+                    //these can be duplicated within child elements, too hard to properly check for
+                } else if (sampleattributesseensample.contains(className)) {
+                    throw new SAXException("SampleAttribute class "+className+" duplicated in "+groupID);
+                } else {
+                    sampleattributesseensample.add(className);
+                }                            
+            } 
+        }
+
+
+        @Override
+        public void endElement(String uri, String localName, String name) throws SAXException {
+            if (name.equals("SampleGroup")) {
+                groupID = null;
+                sampleattributesknown.clear();
+            } else if (name.equals("Sample")) {
+                inSample = false;
+                sampleattributesseen.addAll(sampleattributesseensample);
+                sampleattributesseensample.clear();
+            } else if (name.equals("SampleAttribute")) {
+                //seen but not known
+                for (String attr : sampleattributesseen) {
+                    if (!sampleattributesknown.contains(attr)) {
+                        throw new SAXException("SampleAttribute class "+attr+" is seen but not known "+groupID);   
+                    }
+                }
+                
+                //known but not seen
+                for (String attr : sampleattributesknown) {
+                    if (!sampleattributesseen.contains(attr)) {
+                        throw new SAXException("SampleAttribute class "+attr+" is known but not seen "+groupID);   
+                    }
+                }
+                
+            } 
+        }
+    }
+
+    
+    private class ValidatorRunnable implements Runnable {
+        private final File inputFile;
+        
+        public ValidatorRunnable(File inputFile) {
+            this.inputFile = inputFile;
+        }
+        
+        @Override
+        public void run() {
+            //TODO finish
+            XMLReader xr;
+            BiosamplesHandler handler = new BiosamplesHandler();
+            try {
+                xr = XMLReaderFactory.createXMLReader();
+                xr.setContentHandler(handler);
+                xr.parse(new InputSource(new BufferedReader(new FileReader(inputFile))));
+            } catch (SAXException e) {
+                log.error("Unable to process "+inputFile, e);
+            } catch (FileNotFoundException e) {
+                log.error("Unable to process "+inputFile, e);
+            } catch (IOException e) {
+                log.error("Unable to process "+inputFile, e);
+            }
+        }
+    }
+    
+    @Override
+    protected Runnable getNewTask(File inputFile) {
+        return new ValidatorRunnable(inputFile);
+    }
+    
+    public static void main(String[] args) {
+        new Validator().doMain(args);
+    }
+
+}
