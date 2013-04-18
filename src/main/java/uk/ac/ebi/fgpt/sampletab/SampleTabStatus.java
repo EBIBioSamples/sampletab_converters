9f0950adc6acbcb1601388c975bcf9558c5a9461
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-04-18 15:07:48 +0000

    Update SampleTabStatus to avoid AGE stuff and only do FTP, brought up-to-date with AbstractInfileDriver usage

---
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/SampleTabStatus.java
@@ -0,0 +1,39 @@
+package uk.ac.ebi.fgpt.sampletab;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.kohsuke.args4j.Option;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class SampleTabStatus extends AbstractInfileDriver<SampleTabStatusRunnable> {
+
+    @Option(name = "-f", aliases={"--ftp"}, usage = "ftp directory")
+    private File ftpDirFile;
+    
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    public SampleTabStatus() {
+        Properties properties = new Properties();
+        try {
+            properties.load(SampleTabStatus.class.getResourceAsStream("/sampletabconverters.properties"));
+        } catch (IOException e) {
+            log.error("Unable to read resource age.properties", e);
+        }
+        ftpDirFile = new File(properties.getProperty("biosamples.ftp.path"));
+    }
+    
+    public static void main(String[] args) {
+        new SampleTabStatus().doMain(args);
+    }
+
+    @Override
+    protected SampleTabStatusRunnable getNewTask(File inputFile) {
+        return new SampleTabStatusRunnable(inputFile, ftpDirFile);
+    }
+}
