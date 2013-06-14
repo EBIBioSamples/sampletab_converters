ebbb807551c003bafe8558e94509c93fddde5121
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-06-14 13:54:12 +0000

    Improved extra validation checks

--- src/main/java/uk/ac/ebi/fgpt/sampletab/tools/ValidationDriver.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/tools/ValidationDriver.java
@@ -1,12 +1,10 @@
 package uk.ac.ebi.fgpt.sampletab.tools;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.kohsuke.args4j.Option;
 import org.mged.magetab.error.ErrorItem;
 import org.mged.magetab.error.ErrorItemImpl;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +12,7 @@ import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ValidateException;
 import uk.ac.ebi.arrayexpress2.magetab.listener.ErrorItemListener;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.SampleData;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Publication;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.SampleNode;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.CharacteristicAttribute;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.CommentAttribute;
@@ -47,7 +46,6 @@ public class ValidationDriver extends AbstractInfileDriver {
             });
         }
         
-        
         @Override
         public void run() {
             SampleData sampledata = null;
@@ -66,7 +64,7 @@ public class ValidationDriver extends AbstractInfileDriver {
         public synchronized void validate(SampleData sampledata) throws ValidateException {
             super.validate(sampledata);
             
-            //some warnings...
+            //some basic errors...
             if (sampledata.msi.submissionTitle == null){
                 fireErrorItemEvent(new ErrorItemImpl("Submission Title is null", -1, getClass().getName()));
             } else if (sampledata.msi.submissionTitle.length() < 10){
@@ -78,11 +76,32 @@ public class ValidationDriver extends AbstractInfileDriver {
             } else if (sampledata.msi.submissionDescription.length() < 10){
                 fireErrorItemEvent(new ErrorItemImpl("Submission Description is under 10 characters long", -1, getClass().getName()));
             }
+
+            if (!sampledata.msi.submissionIdentifier.startsWith("G")) {
+                fireErrorItemEvent(new ErrorItemImpl("Submission Identifier does not start with G", -1, getClass().getName()));
+            }
+            if (sampledata.msi.submissionIdentifier.length() < 3) {
+                fireErrorItemEvent(new ErrorItemImpl("Submission Identifier is under 3 characters long", -1, getClass().getName()));
+            }
+            
+            for (Publication p : sampledata.msi.publications) {
+                Integer i = null;
+                try {
+                    i = Integer.parseInt(p.getPubMedID());
+                } catch (NumberFormatException e) {
+                    fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number '"+p.getPubMedID()+"'", -1, getClass().getName()));
+                }
+                if (i != null) {
+                    if (!p.getPubMedID().equals(i.toString())) {
+                        fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number "+p.getPubMedID(), -1, getClass().getName()));
+                    }
+                }
+            }
             
             
             for (SampleNode s : sampledata.scd.getNodes(SampleNode.class)) {
                 
-                //if the node has no descriptions, check none of the attribtues contain the string "description"
+                //if the node has no descriptions, check none of the attributes contain the string "description"
                 if (s.getSampleDescription() == null || s.getSampleDescription().length() == 0) {
                     for (SCDNodeAttribute a : s.getAttributes()) {
                         
@@ -116,15 +135,49 @@ public class ValidationDriver extends AbstractInfileDriver {
                                 fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has only secondary description", -1, getClass().getName()));
                             }
                         }
-
-                        //check that organisms have a tax id
-                        if (isOrganism) {
-                            OrganismAttribute oa = (OrganismAttribute) a;
-                            if (oa.getTermSourceID() == null || oa.getTermSourceID().length() == 0) {
-                                fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has no Organism Term Source REF", -1, getClass().getName()));
-                            }
+                    }
+                }
+                
+                boolean hasOrganism = false;
+                for (SCDNodeAttribute a : s.getAttributes()) {
+                    boolean isOrganism = false;
+                    synchronized(OrganismAttribute.class) {
+                        isOrganism = OrganismAttribute.class.isInstance(a);
+                    }
+                    boolean isCommentAttribute = false;
+                    synchronized(CommentAttribute.class){
+                        isCommentAttribute = CommentAttribute.class.isInstance(a);
+                    }
+                    boolean isCharacteristicAttribute = false;
+                    synchronized(CharacteristicAttribute.class){
+                        isCharacteristicAttribute = CharacteristicAttribute.class.isInstance(a);
+                    }
+                    
+                    //check that organisms have a tax id
+                    if (isOrganism) {
+                        hasOrganism = true;
+                        OrganismAttribute oa = (OrganismAttribute) a;
+                        if (oa.getTermSourceID() == null || oa.getTermSourceID().length() == 0) {
+                            fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has no Organism Term Source REF for "+oa.getAttributeValue(), -1, getClass().getName()));
                         }
                     }
+                    
+                    //check that comments and characteristics don't have both units and term source refs
+                    if (isCommentAttribute) {
+                        CommentAttribute ca = (CommentAttribute) a;
+                        if (ca.unit != null && ca.getTermSourceREF() != null) {
+                            fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has an attribute with both unit and term source", -1, getClass().getName()));
+                        }
+                    }
+                    if (isCharacteristicAttribute) {
+                        CharacteristicAttribute ca = (CharacteristicAttribute) a;
+                        if (ca.unit != null && ca.getTermSourceREF() != null) {
+                            fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has an attribute with both unit and term source", -1, getClass().getName()));
+                        }
+                    }
+                }
+                if (!hasOrganism) {
+                    fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has no Organism", -1, getClass().getName()));
                 }
             }
         }
