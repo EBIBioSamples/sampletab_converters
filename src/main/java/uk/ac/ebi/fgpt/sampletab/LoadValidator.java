a8660693c90deb0938f94421449ff18ef9ea21b5
faulcon <faulcon@2913f559-6b04-0410-9a09-c530ee9f5186>
2013-07-03 10:02:23 +0000

    Added some field size limits

--- src/main/java/uk/ac/ebi/fgpt/sampletab/LoadValidator.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/LoadValidator.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.graph.Node;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ValidateException;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.SampleData;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Organization;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Person;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.GroupNode;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.SampleNode;
 import uk.ac.ebi.arrayexpress2.sampletab.validator.SampleTabValidator;
@@ -57,6 +59,27 @@ public class LoadValidator extends SampleTabValidator {
            if (s.getSampleAccession() == null || !s.getSampleAccession().matches("^SAMEA?[1-9][0-9]$")) {
                fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has a non-EBI accession "+s.getSampleAccession(), -1, getClass().getName()));
            }
+        }
+        
+        //various field length validations for relational database
+        for (Person p : sampledata.msi.persons) {
+            if (p.getEmail() != null && p.getEmail().length() > 60) {
+                fireErrorItemEvent(new ErrorItemImpl("Too long email address "+p.getEmail(), -1, getClass().getName()));
+            }
+            if (p.getFirstName() != null && p.getFirstName().length() > 60) {
+                fireErrorItemEvent(new ErrorItemImpl("Too long first name "+p.getFirstName(), -1, getClass().getName()));
+            }
+            if (p.getInitials() != null && p.getInitials().length() > 30) {
+                fireErrorItemEvent(new ErrorItemImpl("Too long initials "+p.getInitials(), -1, getClass().getName()));
+            }
+            if (p.getLastName() != null && p.getLastName().length() > 60) {
+                fireErrorItemEvent(new ErrorItemImpl("Too long last name "+p.getLastName(), -1, getClass().getName()));
+            }
+        }
+        for (Organization o : sampledata.msi.organizations) {
+            if (o.getName() != null && o.getName().length() > 200) {
+                fireErrorItemEvent(new ErrorItemImpl("Too long organization name "+o.getEmail(), -1, getClass().getName()));
+            }
         }
     }
 }
--- src/main/java/uk/ac/ebi/fgpt/sampletab/tools/ValidationDriver.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/tools/ValidationDriver.java
@@ -8,10 +8,13 @@ import org.mged.magetab.error.ErrorItemImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.ebi.arrayexpress2.magetab.datamodel.graph.Node;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ValidateException;
 import uk.ac.ebi.arrayexpress2.magetab.listener.ErrorItemListener;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.SampleData;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Organization;
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Person;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.msi.Publication;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.SampleNode;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.scd.node.attribute.CharacteristicAttribute;
@@ -59,8 +62,38 @@ public class ValidationDriver extends AbstractInfileDriver {
         
     }
     
+    
     private class ImprovedSampleTabValidator  extends SampleTabValidator {
 
+        private boolean hasOrganism (SampleNode s ) {
+            
+            for (SCDNodeAttribute a : s.getAttributes()) {
+                boolean isOrganism = false;
+                synchronized(OrganismAttribute.class) {
+                    isOrganism = OrganismAttribute.class.isInstance(a);
+                }
+                if (isOrganism) {
+                    return true;
+                }
+            }
+            
+            for (Node p : s.getParentNodes()){
+                boolean isSample = false;
+                synchronized(SampleNode.class) {
+                    isSample = SampleNode.class.isInstance(p);
+                }
+                if (isSample) {
+                    SampleNode ps = (SampleNode) p;
+                    if (hasOrganism(ps)){
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+                
+                
         public synchronized void validate(SampleData sampledata) throws ValidateException {
             super.validate(sampledata);
             
@@ -84,17 +117,41 @@ public class ValidationDriver extends AbstractInfileDriver {
                 fireErrorItemEvent(new ErrorItemImpl("Submission Identifier is under 3 characters long", -1, getClass().getName()));
             }
             
+            //validate pub med ids
             for (Publication p : sampledata.msi.publications) {
                 Integer i = null;
-                try {
-                    i = Integer.parseInt(p.getPubMedID());
-                } catch (NumberFormatException e) {
-                    fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number '"+p.getPubMedID()+"'", -1, getClass().getName()));
-                }
-                if (i != null) {
-                    if (!p.getPubMedID().equals(i.toString())) {
-                        fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number "+p.getPubMedID(), -1, getClass().getName()));
+                if (p.getPubMedID() != null && p.getPubMedID().length() > 0) {
+                    try {
+                        i = Integer.parseInt(p.getPubMedID());
+                    } catch (NumberFormatException e) {
+                        fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number '"+p.getPubMedID()+"'", -1, getClass().getName()));
                     }
+                    if (i != null) {
+                        if (!p.getPubMedID().equals(i.toString())) {
+                            fireErrorItemEvent(new ErrorItemImpl("PubMed ID is not a number "+p.getPubMedID(), -1, getClass().getName()));
+                        }
+                    }
+                }
+            }
+            
+            //various field length validations for relational database
+            for (Person p : sampledata.msi.persons) {
+                if (p.getEmail() != null && p.getEmail().length() > 60) {
+                    fireErrorItemEvent(new ErrorItemImpl("Too long email address "+p.getEmail(), -1, getClass().getName()));
+                }
+                if (p.getFirstName() != null && p.getFirstName().length() > 60) {
+                    fireErrorItemEvent(new ErrorItemImpl("Too long first name "+p.getFirstName(), -1, getClass().getName()));
+                }
+                if (p.getInitials() != null && p.getInitials().length() > 30) {
+                    fireErrorItemEvent(new ErrorItemImpl("Too long initials "+p.getInitials(), -1, getClass().getName()));
+                }
+                if (p.getLastName() != null && p.getLastName().length() > 60) {
+                    fireErrorItemEvent(new ErrorItemImpl("Too long last name "+p.getLastName(), -1, getClass().getName()));
+                }
+            }
+            for (Organization o : sampledata.msi.organizations) {
+                if (o.getName() != null && o.getName().length() > 200) {
+                    fireErrorItemEvent(new ErrorItemImpl("Too long organization name "+o.getEmail(), -1, getClass().getName()));
                 }
             }
             
@@ -137,8 +194,10 @@ public class ValidationDriver extends AbstractInfileDriver {
                         }
                     }
                 }
+
+                boolean hasLatitude = false;
+                boolean hasLongitude = false;
                 
-                boolean hasOrganism = false;
                 for (SCDNodeAttribute a : s.getAttributes()) {
                     boolean isOrganism = false;
                     synchronized(OrganismAttribute.class) {
@@ -155,7 +214,6 @@ public class ValidationDriver extends AbstractInfileDriver {
                     
                     //check that organisms have a tax id
                     if (isOrganism) {
-                        hasOrganism = true;
                         OrganismAttribute oa = (OrganismAttribute) a;
                         if (oa.getTermSourceID() == null || oa.getTermSourceID().length() == 0) {
                             fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has no Organism Term Source REF for "+oa.getAttributeValue(), -1, getClass().getName()));
@@ -163,10 +221,17 @@ public class ValidationDriver extends AbstractInfileDriver {
                     }
                     
                     //check that comments and characteristics don't have both units and term source refs
+                    //check for latitude & longitude
                     if (isCommentAttribute) {
                         CommentAttribute ca = (CommentAttribute) a;
                         if (ca.unit != null && ca.getTermSourceREF() != null) {
                             fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has an attribute with both unit and term source", -1, getClass().getName()));
+                        }
+                        if (ca.type.equalsIgnoreCase("latitude")) {
+                            hasLatitude = true;
+                        }
+                        if (ca.type.equalsIgnoreCase("longitude")) {
+                            hasLongitude = true;
                         }
                     }
                     if (isCharacteristicAttribute) {
@@ -174,10 +239,23 @@ public class ValidationDriver extends AbstractInfileDriver {
                         if (ca.unit != null && ca.getTermSourceREF() != null) {
                             fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has an attribute with both unit and term source", -1, getClass().getName()));
                         }
+                        if (ca.type.equalsIgnoreCase("latitude")) {
+                            hasLatitude = true;
+                        }
+                        if (ca.type.equalsIgnoreCase("longitude")) {
+                            hasLongitude = true;
+                        }
                     }
                 }
-                if (!hasOrganism) {
+                
+                //check for organism attribute
+                //also check nodes upstream for inheritance
+                if (!hasOrganism(s)) {
                     fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has no Organism", -1, getClass().getName()));
+                }
+                
+                if (hasLatitude != hasLongitude) {
+                    fireErrorItemEvent(new ErrorItemImpl("Sample "+s.getNodeName()+" has unblanaced lat/long", -1, getClass().getName()));   
                 }
             }
         }
