b9954d7f0857287418a6c5be4bb97eae5572058c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2013-07-16 11:59:55 +0100

    Enabled taxon test previously disabled due to proxy issues

--- src/test/java/uk/ac/ebi/fgpt/sampletab/TestTaxonUtils.java
+++ src/test/java/uk/ac/ebi/fgpt/sampletab/TestTaxonUtils.java
@@ -14,25 +14,25 @@ public class TestTaxonUtils extends TestCase {
     private Logger log = LoggerFactory.getLogger(getClass());
 
     public void testHuman() {
-        /*
+        
         try {
             assertEquals("Homo sapiens", TaxonUtils.getSpeciesOfID(9606));
         } catch (TaxonException e) {
             log.error("Problem getting species of ID 9606", e);
             fail();
         }
-        */
+        
     }
     
     public void testXenopus() {
-        /*
+        
         try {
             assertEquals(new Integer(8364), TaxonUtils.findTaxon("Xenopus (Silurana) tropicalis"));
         } catch (TaxonException e) {
             log.error("Problem getting tax id of 'Xenopus (Silurana) tropicalis'", e);
             fail();
         }
-        */
+        
     }
 
 }
