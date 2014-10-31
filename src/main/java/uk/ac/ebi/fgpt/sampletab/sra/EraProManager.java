f4a606253b9e4ba6ee9510968efa1dbbc6ef664e
Adam Faulconbridge <afaulconbridge@googlemail.com>
2014-10-31 11:36:01 +0000

    Added bulk retrieval of ena/biosample mappings

--- src/main/java/uk/ac/ebi/fgpt/sampletab/sra/EraProManager.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/sra/EraProManager.java
@@ -9,7 +9,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -304,5 +306,78 @@ select * from cv_status;
 	
         return sampleIds;
 	}
+	
+
+	/**
+	 * Retuns a mapping between ENA sample ID and BioSample ID
+	 * Only looks for sample updated or released within a specific date window 
+	 * 
+	 * 
+	 * @param minDate
+	 * @param maxDate
+	 * @return
+	 */
+    public Map<String, String> getSRABioSampleMappingForDateRange(Date minDate, Date maxDate) {
+        PreparedStatement statement = null;
+        Connection connection = null;
+        ResultSet resultset = null;
+        log.info("Getting all public sample - biosample ids");
+        String query = "SELECT SAMPLE_ID, BIOSAMPLE_ID FROM SAMPLE WHERE STATUS_ID = 4 AND EGA_ID IS NULL "
+                + "AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?))";
+
+        Map<String, String> sampleIds = new HashMap<String, String>();
+
+        try {
+            BoneCPDataSource datasource = getDataSource();
+            connection = datasource.getConnection();
+            statement = connection.prepareStatement(query);
+            statement.setDate(1, new java.sql.Date(minDate.getTime()));
+            statement.setDate(2, new java.sql.Date(maxDate.getTime()));
+            statement.setDate(3, new java.sql.Date(minDate.getTime()));
+            statement.setDate(4, new java.sql.Date(maxDate.getTime()));
+            resultset = statement.executeQuery();
+            if (resultset == null) {
+                log.info("No Updates for sample mapping during the time period provided");
+            } else {
+                while (resultset.next()) {
+                    String sampleId = resultset.getString(1); // result sets are one-indexed,
+                    String biosampleId = resultset.getString(2);
+                    sampleIds.put(sampleId, biosampleId);
+                }
+            }
+        } catch (SQLException e) {
+            log.error("Problem acessing database", e);
+        } catch (ClassNotFoundException e) {
+            log.error("The BoneCPDatasouce class for connection to the database cannot be found", e);
+        } finally {
+            // close each of these separately in case of errors
+            if (resultset != null) {
+                try {
+                    resultset.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    // do nothing
+                }
+            }
+        }
+
+        log.info("Got " + sampleIds.size() + " public sample ids");
+
+        return sampleIds;
+
+    }
 
 }
