7a91f8a104b3d4768315d77fec12701687a17522
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-05-12 08:39:38 +0100

    Simplified, individual processes will handle out of date ness

--- src/main/java/uk/ac/ebi/fgpt/sampletab/SampleTabConanDriver.java
+++ src/main/java/uk/ac/ebi/fgpt/sampletab/SampleTabConanDriver.java
@@ -31,16 +31,6 @@ public class SampleTabConanDriver extends AbstractInfileDriver<SampleTabConanRun
         File subdir = inputFile.getAbsoluteFile().getParentFile();
         String submissionID = subdir.getName();
         
-        File sampletabpre = new File(subdir, "sampletab.pre.txt");
-        File sampletab = new File(subdir, "sampletab.txt");
-        File sampletabtoload = new File(subdir, "sampletab.toload.txt");
-        
-        if (!sampletab.exists() || sampletabpre.lastModified() > sampletab.lastModified()) {
-            return new SampleTabConanRunnable(submissionID, "BioSamples (other)", 0);
-        } else if (!sampletabtoload.exists() || sampletab.lastModified() > sampletabtoload.lastModified()) {
-            return new SampleTabConanRunnable(submissionID, "BioSamples (other)", 1);
-        }
-        
-        return null;
+        return new SampleTabConanRunnable(submissionID, "BioSamples (other)", 0);
     }
 }
